3f6952062bfc9ce2aceb371b63c1a97e76d4d59b
Joshua Hursey <jjhursey@cs.uwlax.edu>
2015-07-09 12:43:41 -0500

    Add a status check script. Make the test longer so that status will show up.
    There is a timing delay when the output.txt file gets sync'ed to the local file system. This seems to be an NFS issue since if you are on the first allocated node then you can see the file update in real time. But since we are accessing the file from the headnode there is a delay when NFS decides to sync it. It's not a big problem at the moment, but something to lookout for.

--- modules/mpi-ring/bin/onramp_status.py
+++ modules/mpi-ring/bin/onramp_status.py
@@ -7,11 +7,32 @@
 # - onramp_run_params.ini file is available in current working directory
 #
 import sys
+import re
 
 #
 # Display any special message you want the user to see, or leave blank if nothing.
 # Please restrict status messages to 1 line of text.
 #
+
+# Read in the output file
+lines = [line.rstrip('\n') for line in open('onramp/output.txt')]
+
+# If the file is empty then nothing to do
+if len(lines) <= 0:
+    sys.exit(0)
+
+
+#print "Status) Number of Lines: %d" % len(lines)
+
+# Find the last line of 'Increment value'
+last_status = None
+for line in lines:
+    searchObj = re.search( r'Increment value (.*)', line)
+    if searchObj:
+        last_status = searchObj.group(1).strip()
+
+if last_status is not None:
+    print "%s" % last_status
 
 #
 # Exit successfully
