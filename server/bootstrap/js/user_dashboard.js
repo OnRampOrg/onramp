8dce984f6bdde26affdcac7f86b8b8cade9dd48d
Samantha Foley <sfoley@uwlax.edu>
2015-07-28 16:29:16 -0500

    able to get jobs data from server, parse it and display to user

--- server/bootstrap/js/user_dashboard.js
+++ server/bootstrap/js/user_dashboard.js
@@ -28,11 +28,11 @@ function Workspace(data){
 
 function UserDashboardViewModel() {
 	var self = this;
-	self.username = sessionStorage['UserID'];  // want to get this from the cookie/session/server
+	self.username = ko.observable("test");  // want to get this from the cookie/session/server
 	self.userID = sessionStorage['UserID'];
 	self.auth_data = sessionStorage['auth_data'];
 	self.Workspacelist = ko.observableArray([new Workspace({'WorkspaceID': 1, 'WorkspaceName': 'default', 'Description':'My personal workspace'}), new Workspace({'WorkspaceID': 2, 'WorkspaceName': 'test', 'Description':'Hello world'})]);  // want to get this from the server
-	self.Jobslist = ko.observableArray([new Job({'JobID' : 2, 'Workspace' : 'default', 'PCE' : 'flux', 'Module' : 'Hello World', 'RunName' : 'test1', 'Status' : 'Running', 'Runtime' : '0:00'})]);
+	self.Jobslist = ko.observableArray();
 
 	//this.Jobslist.add(new Job({'JobID' : 2, 'Workspace' : 'default', 'PCE' : 'flux', 'Module' : 'Hello World', 'RunName' : 'test1', 'Status' : 'Running', 'Runtime' : '0:00'}));
 	//this.Workspacelist.add(new Workspace({'WorkspaceID': 1, 'WorkspaceName': 'default', 'Description':'My personal workspace'}));
@@ -43,6 +43,7 @@ function UserDashboardViewModel() {
 	$(document).ready( function () {
 		console.log(self.auth_data);
 		console.log(JSON.stringify(self.auth_data));
+		/*
 		$.getJSON( "http://flux.cs.uwlax.edu/onramp/api/users/" + self.userID + "?apikey=" + JSON.parse(self.auth_data).apikey,
 								self.auth_data,
 								function (data){
@@ -55,23 +56,26 @@ function UserDashboardViewModel() {
 									self.username(data.users.data[1]);
 								}
 							);
-
+*/
 		// get jobs for this user
 		$.getJSON( "http://flux.cs.uwlax.edu/onramp/api/users/" + self.userID + "/jobs?apikey=" + JSON.parse(self.auth_data).apikey,
-								self.auth_data,
+								//self.auth_data,
 								function (data){
 									// {"status": 0,
 									//  "status_message": "Success",
 									//  "users": {
 									//    "fields": ["user_id", "username", "full_name", "email", "is_admin", "is_enabled"],
 									//    "data": [2, "alice", "", "", 0, 1]}}
-									console.log(data);
-									for (raw in data.users.data){
+									console.log(JSON.stringify(data));
+									for (var x = 0; x < data.users.data.length; x++){
+										var raw = data.users.data[x];
+										console.log(raw);
 										var conv_data = {};
 										for(var i = 0; i < data.users.fields.length; i++){
+											console.log("adding: " + data.users.fields[i] + " = " + raw[i]);
 											conv_data[data.users.fields[i]] = raw[i];
 										}
-										self.JobsList.push(new Job(conv_data));
+										self.Jobslist.push(new Job(conv_data));
 									}
 								}
 							);
