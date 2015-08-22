fe596d6b8ab4907a8d08adac1accb3e048fc3a4c
Dan Koepke <koepke.dan2@uwlax.edu>
2015-08-22 14:28:46 -0500

    Added second module to testing suite so that bin scripts could run with out the 10s sleeps needed in the first test module added.

---
+++ pce/src/testing/testmodule2/src/hello.c
@@ -0,0 +1,68 @@
+/*
+ * Sample MPI "hello world" application in C
+ *
+ * J. Hursey
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "mpi.h"
+
+int main(int argc, char* argv[])
+{
+    int rank, size, len;
+    char processor[MPI_MAX_PROCESSOR_NAME];
+    char *name = NULL;
+
+    /*
+     * Initialize the MPI library
+     */
+    MPI_Init(&argc, &argv);
+
+    /*
+     * Check to see if we have a command line argument for the name
+     */
+    if( argc > 1 ) {
+        name = strdup(argv[1]);
+    }
+    else {
+        name = strdup("World");
+    }
+
+    /*
+     * Get my 'rank' (unique ID)
+     */
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    /*
+     * Get the size of the world (How many other 'processes' are there)
+     */
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    /*
+     * Get the processor name (usually the hostname)
+     */
+    MPI_Get_processor_name(processor, &len);
+
+    /*
+     * Print a message from this process
+     */
+    printf("Hello, this will result in deterministic output!");
+
+    /*
+     * Shutdown the MPI library before exiting
+     */
+    MPI_Finalize();
+
+    /*
+     * Cleanup
+     */
+    if( NULL != name ) {
+        free(name);
+        name = NULL;
+    }
+
+    return 0;
+}
