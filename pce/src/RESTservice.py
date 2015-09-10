cb70587a5ff9d6bde4562476bd91aa19f3ca5c63
Dan Koepke <koepke.dan2@uwlax.edu>
2015-09-10 10:56:43 -0500

    Connected cluster info and ping dispatchers to URL endpoints.

--- pce/src/RESTservice.py
+++ pce/src/RESTservice.py
@@ -13,7 +13,8 @@ from cherrypy.process.plugins import Daemonizer, PIDFile
 from configobj import ConfigObj
 from validate import Validator
 
-from PCE.dispatchers import APIMap, Cluster, Files, Jobs, Modules
+from PCE.dispatchers import APIMap, ClusterInfo, ClusterPing, Files, Jobs, \
+                            Modules
 
 
 def _CORS():
@@ -118,7 +119,8 @@ if __name__ == '__main__':
     cherrypy.tools.CORS = cherrypy.Tool('before_finalize', _CORS)
     cherrypy.tree.mount(Modules(ini, log_name), '/modules', conf)
     cherrypy.tree.mount(Jobs(ini, log_name), '/jobs', conf)
-    cherrypy.tree.mount(Cluster(ini, log_name), '/cluster', conf)
+    cherrypy.tree.mount(ClusterInfo(ini, log_name), '/cluster/info', conf)
+    cherrypy.tree.mount(ClusterPing(ini, log_name), '/cluster/ping', conf)
     cherrypy.tree.mount(Files(ini, log_name), '/files', conf)
     cherrypy.tree.mount(APIMap(ini, log_name), '/api', conf)
 
