5f3fb65b17b4f392b50706469e663a8314b41c3f
Dan Koepke <koepke.dan2@uwlax.edu>
2015-10-15 20:49:47 -0500

    Updated service_test to use current module folder structure.

--- pce/src/testing/service_test/__init__.py
+++ pce/src/testing/service_test/__init__.py
@@ -6,14 +6,16 @@ import unittest
 from configobj import ConfigObj
 from subprocess import CalledProcessError, check_output
 
-__test__ = False
+from PCEHelper import pce_root
+
+#__test__ = False
 
 class TestModtest(unittest.TestCase):
 
     def setUp(self):
         self.ret_dir = os.getcwd()
-        os.chdir('../../')
-        self.conf = ConfigObj('src/testing/testmodule/testing/modtest.cfg',
+        os.chdir(pce_root)
+        self.conf = ConfigObj('src/testing/testmodule/tests/modtest.cfg',
                               configspec='src/configspecs/modtest.cfgspec')
         dummy_filepath = 'src/testing/testmodule/testing/dummy_runparams.cfg'
         self.dummy_cfg = ConfigObj(dummy_filepath)
@@ -65,8 +67,8 @@ class TestModtest(unittest.TestCase):
         time.sleep(30)
 
     def modtest_test_noclean_noverbose_noerror(self):
-        output = check_output(['./onramp_pce_service.py', 'modtest',
-                               'src/testing/testmodule/testing/modtest.cfg'])
+        output = check_output(['bin/onramp_pce_service.py', 'modtest',
+                               'src/testing/testmodule/tests/modtest.cfg'])
         self.assertEqual(output, self.out_noclean_noverbose_noerror)
 
         # Verify onramp_runparams matches self.dummy_cfg
@@ -87,8 +89,8 @@ class TestModtest(unittest.TestCase):
                 self.assertTrue(res)
 
     def modtest_test_noclean_verbose_noerror(self):
-        output = check_output(['./onramp_pce_service.py', 'modtest', '-v',
-                               'src/testing/testmodule/testing/modtest.cfg'])
+        output = check_output(['bin/onramp_pce_service.py', 'modtest', '-v',
+                               'src/testing/testmodule/tests/modtest.cfg'])
 
         for line in self.out_noclean_verbose_noerror.split('\n'):
             self.assertIn(line, output)
@@ -111,8 +113,8 @@ class TestModtest(unittest.TestCase):
                 self.assertTrue(res)
 
     def modtest_test_clean_verbose_noerror(self):
-        output = check_output(['./onramp_pce_service.py', 'modtest', '-v',
-                               'src/testing/testmodule/testing/modtest-clean.cfg'])
+        output = check_output(['bin/onramp_pce_service.py', 'modtest', '-v',
+                               'src/testing/testmodule/tests/modtest-clean.cfg'])
 
         for line in self.out_clean_verbose_noerror.split('\n'):
             self.assertIn(line, output)
