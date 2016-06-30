b835233eac916e6af81ad1f700949c539ba66704
Samantha Foley <sfoley@uwlax.edu>
2016-06-30 20:57:54 +0000

    Revert "most of the way to fixing the PBS scheduler.  do not forget to take out the debugging output"
    This reverts commit 8af55ea874bdccd7aee12c3bad49e9a0a7496f88.

--- pce/src/PCE/tools/schedulers.py
+++ pce/src/PCE/tools/schedulers.py
@@ -276,20 +276,18 @@ class PBSScheduler(_BatchScheduler):
         Returns:
             Batch script implementing given attrs.
         """
-        self.logger.debug("starting to write file")
         script = '#!/bin/bash\n'
         script += '\n'
         script += '################################################\n'
-        script += '#PBS -l select=' + num_nodes + ':mpiprocs='+ numtasks +'\n'
-        script += '#PBS -N ' + run_name + '\n' 
+        script += '#PBS -l select=%d:mpiprocs=%d\n' % (num_nodes, numtasks)
+        script += '#PBS -N %s\n' % run_name
         script += '#PBS -V\n'
         script += '#PBS -j oe\n'
         script += '#PBS -o output.txt\n'
         script += '################################################\n'
         script += '\n'
         script += 'cd ${PBS_O_WORKDIR}\n'
-        script +=  self.local_python + ' bin/onramp_run.py\n'
-        self.logger.debug("wrote: %s" % script)
+        script += '%s bin/onramp_run.py\n' % self.local_python
         return script
 
     def schedule(self, proj_loc):
