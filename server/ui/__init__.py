04557666f62f98738ecd8fb6a912d58ff58cba4f
telam <travis.e.elam@gmail.com>
2016-10-22 23:16:04 -0500

    added code to help out with imports

---
+++ server/ui/__init__.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import AppRegistryNotReady
+
+# IMPORTANT: the following code needs to be here so we can import
+# our models without having to call django.setup() in every single
+# file that tries to imports them
+try:
+    from django.apps import apps
+    apps.check_apps_ready()
+except AppRegistryNotReady:
+    import django
+    django.setup()
