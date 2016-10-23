fc17a9542d1fdfbb474c07040310730388ec62aa
telam <travis.e.elam@gmail.com>
2016-10-22 23:15:52 -0500

    simplified path

--- server/ui/wsgi.py
+++ server/ui/wsgi.py
@@ -15,10 +15,9 @@ import site
 site.addsitedir('../virtual-env')
 
 # Add the app's directory to the PYTHONPATH
-cwd = os.getcwd()
-sys.path.append(cwd)
-sys.path.append(cwd.rstrip("/ui"))
-sys.path.append("{}/virtual-env/lib/python2.7/site-packages".format(cwd.rstrip("/ui")))
+sys.path.append("../")
+sys.path.append('../ui')
+sys.path.append("../virtual-env/lib/python2.7/site-packages")
 
 # to set environment settings for Django apps
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ui.settings")
